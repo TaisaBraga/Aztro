dda1c6e759c63135b83e7c132832cbace63e08a1
Taisa Braga Bento <taisabraga.adm@gmail.com>
2022-07-27 21:56:41 -0300

    Create Top Bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import AppTopBar from './component/AppTopBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Aztro API</h1>
+      <AppTopBar/>
     </div>
   );
 }
