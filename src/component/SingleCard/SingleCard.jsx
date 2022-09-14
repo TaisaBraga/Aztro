4b9eff93c5d8926383469feaf56d9f360870f780
taisa.braga <taisabraga.adm@gmail.com>
2022-09-14 14:05:29 -0300

    refact(DIGBRAZ-01): change_style_page_to_material_ui

--- src/component/SingleCard/SingleCard.jsx
+++ src/component/SingleCard/SingleCard.jsx
@@ -1,16 +1,47 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Card, SingleImage } from "./styles";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    backgroundColor: "rgba(183, 176, 176, 0.196)",
+    borderRadius: "1rem",
+    boxShadow: "0.125rem 0.188rem 0.313rem rgba(51, 51, 51, 0.283)",
+    width: "8.125rem",
+    height: "8.125rem",
+    margin: "0.525rem",
+    padding: "0.625rem",
+  },
+  cardImage: {
+    display: "flex",
+    margin: "0 auto",
+    width: "7em",
+    "&:hover": {
+      cursor: "pointer",
+      backgroundColor: "#fff0f5",
+      borderRadius: "1rem",
+    },
+  },
+  cardText: {
+    color: "#DF608C",
+    fontFamily: "'Quicksand', sans-serif",
+    textTransform: "capitalize",
+    textAlign: "center",
+  },
+}));
 
 const SingleCard = ({ signImage, signName }) => {
+  const classes = useStyles();
   const navigate = useNavigate();
 
   return (
-    <Card onClick={() => navigate(`/horoscopo/${signName}`)}>
-      <SingleImage src={signImage} alt={signName} />
-      <p>{signName}</p>
-    </Card>
-    
+    <div
+      className={classes.root}
+      onClick={() => navigate(`/horoscopo/${signName}`)}
+    >
+      <img src={signImage} alt={signName} className={classes.cardImage} />
+      <p className={classes.cardText}>{signName}</p>
+    </div>
   );
 };
 
